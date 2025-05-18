01335fe22a6edfc30acaae5feec4241203e171d5
YoavNEdelist <yoav.edelist@gmail.com>
2025-05-18 11:58:30 +0300

    initial commit

---
+++ api.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+
+
+app = Flask(__name__, template_folder='templates')
+# enviromental/global vars go here
+
+@app.route('')
+def get_weight():
+
+
+
+
+
+
+
+
+
+def post_weight()
