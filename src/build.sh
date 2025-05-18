9f087e06146c3d401cfaa1ea55b07abae231e863
yarden <yardenyg@gmail.com>
2025-05-18 15:16:14 +0300

    fixed yaml file name

--- src/build.sh
+++ src/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Start in detached mode (background)
-docker compose up -d && echo "Container is up and running..."           
+docker compose -f docker-compose-ci.yml up && echo "Container is up and running..."           
 
 # Run for 2 seconds 
 sleep 2
 
 # Stop & remove the container
-docker compose down && echo "Container stopped"            
+docker compose -f docker-compose-ci.yml down && echo "Container stopped"            
 
