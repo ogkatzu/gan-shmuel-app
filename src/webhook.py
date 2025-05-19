03c9cd55316a0312c2dc88a888bfd00b55372816
mayaf94 <maya.f974@gmail.com>
2025-05-19 20:18:40 +0300

    add check method to run the product container

--- src/webhook.py
+++ src/webhook.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # Tries to get the value of an environment variable called GITHUB_SECRET.
 # If the environment variable is not set, it falls back to the default value: 'my_webhook'.
 GITHUB_SECRET = os.environ.get('GITHUB_SECRET', 'my_webhook')
-PORT = 465
+PORT = 587
 PASSWORD = "cdghvoecadvndscn"
 context = ssl.create_default_context()
 smtp_server = "smtp.gmail.com"
@@ -61,42 +61,50 @@ def verify_signature(payload, signature_header):
 
 #     return 'Event ignored', 200 
  
-@app.route('/webhook', methods=['GET'])
-def github_webhook():
-    signature = request.headers.get('X-Hub-Signature-256')
-    payload = request.data
+# @app.route('/webhook', methods=['GET'])
+# def github_webhook():
+#     signature = request.headers.get('X-Hub-Signature-256')
+#     payload = request.data
 
-    if not verify_signature(payload, signature):
-     abort(401, 'Signature verification failed')
+#     if not verify_signature(payload, signature):
+#      abort(401, 'Signature verification failed')
 
-    data = request.json
-    repository = data.get('repository', {})
-    repo_url = repository.get('clone_url')
-    sender_login = payload['sender']['login']
-    #receiver_email = users[sender_login]
-    run_and_build_environment(repo_url)
+#     data = request.json
+#     repository = data.get('repository', {})
+#     repo_url = repository.get('clone_url')
+#     sender_login = payload['sender']['login']
+#     #receiver_email = users[sender_login]
+#     run_and_build_environment(repo_url)
 
-    return 'Pull request is in process...', 200
+#     return 'Pull request is in process...', 200
 
-def run_and_build_environment(repo_url):
-    repo_dir = 'gan-shmuel-app'
+# def run_and_build_environment(repo_url):
+#     repo_dir = 'gan-shmuel-app'
 
-    if not os.path.exists(repo_dir):
-        print("Cloning repository...")
-        subprocess.run(['git', 'clone', '--branch', 'main', str(repo_url), repo_dir], check=True)
-    else:
-        print("Repository already exists. Fetching latest changes...")
-        subprocess.run(['git', '-C', repo_dir, 'fetch'], check=True)
+#     if not os.path.exists(repo_dir):
+#         print("Cloning repository...")
+#         subprocess.run(['git', 'clone', '--branch', 'main', str(repo_url), repo_dir], check=True)
+#     else:
+#         print("Repository already exists. Fetching latest changes...")
+#         subprocess.run(['git', '-C', repo_dir, 'fetch'], check=True)
 
-        subprocess.run(['docker', 'compose', '-f', 'docker-compose-deploy.yaml', 'up'], check=True)
+#         #subprocess.run(['docker', 'compose', '-f', 'docker-compose-deploy.yaml', 'up'], check=True)
 
-    context = ssl.create_default_context()
-with smtplib.SMTP(smtp_server, PORT) as server:
-     server.ehlo()  # Can be omitted
-     server.starttls(context=context)
-     server.ehlo()  # Can be omitted
-     server.login(sender_email, PASSWORD)
-     server.sendmail(sender_email, receiver_email, message)
+#     context = ssl.create_default_context()
+# with smtplib.SMTP(smtp_server, PORT) as server:
+#      server.ehlo()  # Can be omitted
+#      server.starttls(context=context)
+#      server.ehlo()  # Can be omitted
+#      server.login(sender_email, PASSWORD)
+#      server.sendmail(sender_email, receiver_email, message)
+def cheking_docker_up():
+    print("getting the webhook http merge request")
+    print("verify signature")
+    print("checking if there is a local repo")
+    print("clone the repo now")
+    subprocess.run(['git', 'clone', '--branch', 'main', "https://github.com/ogkatzu/gan-shmuel-app.git", "gan-shmuel-app"], check=True)
+    print("running container")
+    subprocess.run(['docker', 'compose', '-f', 'docker-compose-deploy.yaml', 'up'], check=True)
 
 
 if __name__ == '__main__':
